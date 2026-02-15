2bbf944c16f9bba119bb57aa24e914cef8c00b72
TechCrent <ciicicrentsil@gmail.com>
2026-02-15 18:16:05 +0000

    Mock structure 1

---
+++ form-validation/src/Form.jsx
@@ -0,0 +1,29 @@
+
+function Form(){
+    return(
+        <>
+            <div>
+                <p>Order/delivery Tracking</p>
+                <p>Trace your order</p>
+            </div>
+            <div>
+                <p>Personal Information</p>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <div>
+                <p>Order Information</p>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </div>
+        </>
+    );
+}
+
+export default Form
