acea34dd022dc7447ad7c10b3200675c91c74ddf
TechCrent <ciicicrentsil@gmail.com>
2026-02-26 23:56:45 +0000

    commit -r

--- form-validation/src/Validated.jsx
+++ form-validation/src/Validated.jsx
@@ -1,15 +1,14 @@
-function Validated(){
-    return(
-<div className="min-h-screen flex items-center justify-center bg-gray-50">
-  <div className="bg-white p-10 rounded-2xl shadow-lg text-center max-w-sm w-full">
-    <h2 className="text-3xl font-semibold text-green-600">
-      Form Submitted!
-    </h2>
-  </div>
-</div>
+function Validated() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="bg-white p-10 rounded-2xl shadow-lg text-center max-w-sm w-full">
+        <h2 className="text-3xl font-semibold text-green-600">
+          Form Submitted!
+        </h2>
+      </div>
+    </div>
 
-    );
+  );
 }
 
 export default Validated;
-//Comment
